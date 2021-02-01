c020da78aa007793a31dfe9a1649a486279f76e4
Jingwei Ye <yjjw97@gmail.com>
2021-01-31 21:12:08 -0800

    Reusable Routing Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-//import ReactDOM from 'react-dom';
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const items = [
     {
@@ -59,13 +59,28 @@ const showTranslate = () => {
     }
 };
 
+
 export default() => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
-            {showAccordion()}
-            {showList()}
-            {showDropdown()}
-            {showTranslate()}
+            <Route path="/">
+                <Accordion items={items}/>
+            </Route>
+            <Route path='/list'>
+                <Search />
+            </Route>
+            <Route path='/dropdown'>
+                <Dropdown
+                    label='Select a color'
+                    options = {options}
+                    selected = {selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path='/translate'>
+                <Translate />
+            </Route>
         </div>
     );
 };

0289cd8e73caa6cd7bab231015960f92f81c220e
Jingwei Ye <yjjw97@gmail.com>
2021-01-31 22:22:43 -0800

    Handling Command/Ctrl Click

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Link = ({className, href, children}) => {
     const onClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
         event.preventDefault();
         window.history.pushState({}, '', href);
 
